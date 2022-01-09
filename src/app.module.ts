246e375e239ce3839cf392fcc9af17ba17704c33
SuperBluestar <alexander116gm@gmail.com>
2022-01-09 23:12:40 +0900

    nest middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { AdminControllerController } from './admin-controller/admin-controller.controller';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController, AdminControllerController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('cats');
+      // .forRoutes({ path: 'ab*cd', method: RequestMethod.ALL });
+      // .forRoutes({ path: 'cats', method: RequestMethod.GET });
+      // .forRoutes(CatsController);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Request, Response, NextFunction } from 'express';
+
+// Global middleware
+function logger(req: Request, res: Response, next: NextFunction) {
+  console.log(`Request...`);
+  next();
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(logger)
   await app.listen(3000);
 }
 bootstrap();
